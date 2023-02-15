b7235d80a019ee640aa649b75ac5374a3fb04744
rqda <83322694+rqdaA@users.noreply.github.com>
2023-02-15 19:19:32 +0900

    add fraction style Shutter Speed

--- react/src/components/main/common/api.ts
+++ react/src/components/main/common/api.ts
@@ -1,7 +1,7 @@
-import {deviceIdToDBName} from "../../../settings";
+import { deviceIdToDBName } from "../../../settings"
 
 type StorageElements =
-    "lastFetchDate"
+    | "lastFetchDate"
     | "isOnline"
     | "lastUpdate"
     | "temperature"
@@ -15,79 +15,77 @@ type StorageElements =
     | "logs"
 
 interface CaptureSettings {
-    iso: number,
-    f_number: number,
+    iso: number
+    f_number: number
     ss: number
 }
 
 interface GraphData {
-    'date': string,
-    'val': number
+    date: string
+    val: number
 }
 
-const validateInfo = (raw: number): string => (raw !== -128 ? `${raw}` : "データなし");
-const validateUsage = (raw: number): string => (raw !== -128 ? `${raw}%` : '0%')
+const url: string = window.location.hostname
+const validateInfo = (raw: number): string => (raw !== -128 ? `${raw}` : "データなし")
+const validateUsage = (raw: number): string => (raw !== -128 ? `${raw}%` : "0%")
 export const getElement = (deviceId: string | number, elementName: StorageElements): string | undefined => sessionStorage.getItem(`${deviceId}_${elementName}`)
-export const setElement = (deviceId: string | number, elementName: StorageElements, newVal: string): void => sessionStorage.setItem(`${deviceId}_${elementName}`, newVal)
+export const setElement = (deviceId: string | number, elementName: StorageElements, newVal: string): void =>
+    sessionStorage.setItem(`${deviceId}_${elementName}`, newVal)
 export const parseGraphData = (data: number[]): GraphData[] => {
     return data.map((val: number, index: number): GraphData => {
-        return {date: `${index + 1}h`, val: val}
+        return { date: `${index + 1}h`, val: val }
     })
 }
 export const fetchData = async (deviceId: string | number): Promise<void> => {
-    if (getElement(deviceId, "lastFetchDate") && (Date.now() - parseInt(getElement(deviceId, "lastFetchDate"))) < 3600 * 1000)
+    if (getElement(deviceId, "lastFetchDate") && Date.now() - parseInt(getElement(deviceId, "lastFetchDate")) < 3600 * 1000)
         return new Promise((resolve) => resolve())
-    const url: string = 'toms-server.tail2925.ts.net'
-    const respInfo = fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/info`, {mode: "cors"})
-    const respLogs = fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/logs`, {mode: "cors"})
-    const respGraph = fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/usages`, {mode: "cors"})
+    const respInfo = fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/info`, { mode: "cors" })
+    const respLogs = fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/logs`, { mode: "cors" })
+    const respGraph = fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/usages`, { mode: "cors" })
     const infoJson: JSON = await (await respInfo).json()
     const logsJson: JSON = await (await respLogs).json()
     const graphDataJson: JSON = await (await respGraph).json()
     setElement(deviceId, "lastFetchDate", Date.now().toString())
-    setElement(deviceId, "isOnline", 'true')
-    setElement(deviceId, "lastUpdate", infoJson['date'].replace('T', ' '))
-    setElement(deviceId, "temperature", validateInfo(infoJson['temperature']))
-    setElement(deviceId, "moisture", validateInfo(infoJson['moisture']))
-    setElement(deviceId, "cpuTemp", validateInfo(infoJson['cpuTemperature']) + '℃')
-    setElement(deviceId, "uptime", validateInfo(infoJson['uptime']))
-    setElement(deviceId, "usbUsage", validateUsage(infoJson['usbUsage']))
-    setElement(deviceId, "sdCardUsage", validateUsage(infoJson['sdCardUsage']))
-    setElement(deviceId, "cpuUsages", graphDataJson['cpuUsage'].join(','))
-    setElement(deviceId, "memUsages", graphDataJson['memUsage'].join(','))
+    setElement(deviceId, "isOnline", "true")
+    setElement(deviceId, "lastUpdate", infoJson["date"].replace("T", " "))
+    setElement(deviceId, "temperature", validateInfo(infoJson["temperature"]))
+    setElement(deviceId, "moisture", validateInfo(infoJson["moisture"]))
+    setElement(deviceId, "cpuTemp", validateInfo(infoJson["cpuTemperature"]) + "℃")
+    setElement(deviceId, "uptime", validateInfo(infoJson["uptime"]))
+    setElement(deviceId, "usbUsage", validateUsage(infoJson["usbUsage"]))
+    setElement(deviceId, "sdCardUsage", validateUsage(infoJson["sdCardUsage"]))
+    setElement(deviceId, "cpuUsages", graphDataJson["cpuUsage"].join(","))
+    setElement(deviceId, "memUsages", graphDataJson["memUsage"].join(","))
     setElement(deviceId, "logs", JSON.stringify(logsJson))
 }
 
 export const fetchCameraSettings = async (deviceId: string | number): Promise<CaptureSettings> => {
-    const url: string = 'toms-server.tail2925.ts.net'
     const settings = fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/camera_settings`)
     let captureSettings = await (await settings).json()
-    captureSettings['ss'] = parseFloat(captureSettings['ss'].match(/[\d\.]+/)?.[0]) * 1000
-    captureSettings['f_number'] = captureSettings['f_number'].replace('f/', '')
+    captureSettings["ss"] = parseFloat(captureSettings["ss"].match(/[\d\.]+/)?.[0])
+    captureSettings["f_number"] = captureSettings["f_number"].replace("f/", "")
     return captureSettings
 }
 export const fetchLatestImage = async (deviceId: string | number): Promise<string> => {
-    const url: string = 'toms-server.tail2925.ts.net'
     const img = await fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/latest_image`, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'force-cache',
+        method: "POST",
+        mode: "cors",
+        cache: "force-cache",
     })
     return URL.createObjectURL(await img.blob())
 }
 
 export const captureImage = async (deviceId: string | number, settings: CaptureSettings): Promise<string> => {
-    const url: string = 'toms-server.tail2925.ts.net'
     const img = await fetch(`http://${url}:8080/${deviceIdToDBName[deviceId]}/capture`, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'force-cache',
-        headers: {'content-type': 'application/json'},
+        method: "POST",
+        mode: "cors",
+        cache: "force-cache",
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({
-            'iso': settings['iso'].toString(),
-            'f_number': `f/${settings['f_number']}`,
-            'ss': `${settings['ss'] / 1000}s` // Web Input is in MilliSec, GPhoto's input is in Sec
-        })
+            iso: settings["iso"].toString(),
+            f_number: `f/${settings["f_number"]}`,
+            ss: `${settings["ss"]}s`, // Web Input is in MilliSec, GPhoto's input is in Sec
+        }),
     })
     return URL.createObjectURL(await img.blob())
 }
--- react/src/components/main/detailView/control/Control.jsx
+++ react/src/components/main/detailView/control/Control.jsx
@@ -1,58 +1,74 @@
-import React, {useEffect, useState} from 'react';
-import './control.scss'
-import Form from './Form'
-import Button from "./Button";
-import {captureImage, fetchCameraSettings, fetchLatestImage} from "../../common/api.ts";
-import {fNumberList, isoList, ssList} from "../../../../settings";
-import Snackbar from "@mui/material/Snackbar";
+import React, { useEffect, useState } from "react"
+import "./control.scss"
+import Form from "./Form"
+import Button from "./Button"
+import { captureImage, fetchCameraSettings, fetchLatestImage } from "../../common/api.ts"
+import { fNumberList, isoList, ssList, ssFractList } from "../../../../settings"
+import Snackbar from "@mui/material/Snackbar"
 import Alert from "@mui/material/Alert"
-import Loading from "../../common/Loading";
+import Loading from "../../common/Loading"
 
-function Control({deviceId}) {
+function Control({ deviceId }) {
     const [loaded, setLoaded] = useState(false)
     const [cameraSettings, setCameraSettings] = useState({
-        'ss': 0,
-        'iso': 0,
-        'f_number': 0
+        ss: '',
+        iso: 0,
+        f_number: 0,
     })
-    const [imageURL, setImageURL] = useState('')
-    const [errorText, setErrorText] = useState('')
+    const [imageURL, setImageURL] = useState("")
+    const [errorText, setErrorText] = useState("")
 
-    useEffect(
-        () => {
-            const retrieve = async () => {
-                const settings = await fetchCameraSettings(deviceId)
-                setImageURL(await fetchLatestImage(deviceId))
-                setCameraSettings(
-                    {
-                        'ss': settings['ss'],
-                        'iso': settings['iso'],
-                        'f_number': settings['f_number']
-                    })
-            }
-            retrieve().then(() => setLoaded(true))
-        }, []
-    )
+    useEffect(() => {
+        const retrieve = async () => {
+            const settings = await fetchCameraSettings(deviceId)
+            setImageURL(await fetchLatestImage(deviceId))
+            setCameraSettings({
+                ss: settings["ss"],
+                iso: settings["iso"],
+                f_number: settings["f_number"],
+            })
+        }
+        retrieve().then(() => setLoaded(true))
+    }, [])
 
     const onFinish = (key, val) => {
         if (!val) {
-            setCameraSettings({...cameraSettings, [key]: ''})
+            setCameraSettings({ ...cameraSettings, [key]: "" })
             return
         }
 
         let ret = -1
         const pickNearest = (target, list) => {
-            const diff = list.map(e => Math.abs(e - target))
+            const diff = list.map((e) => Math.abs(e - parseFloat(target)))
             const smallestIndex = diff.indexOf(Math.min(...diff))
-            return list[smallestIndex]
+            return list[smallestIndex].toString()
         }
-        if (key === 'iso') ret = pickNearest(val, isoList)
-        else if (key === 'f_number') ret = pickNearest(val, fNumberList)
-        else if (key === 'ss') ret = pickNearest(val, ssList)
-        setCameraSettings({...cameraSettings, [key]: ret})
+
+        const pickNearestFraction = (frTarget, frList) => {
+            const convertDemical = (e) => {
+                const sp = e.split("/").map(parseFloat)
+                return sp[0] / sp[1]
+            }
+
+            const demiTarget = convertDemical(frTarget)
+            const demiList = frList.map(convertDemical)
+
+            const diff = demiList.map((e) => Math.abs(e - demiTarget))
+            const smallestIndex = diff.indexOf(Math.min(...diff))
+            return frList[smallestIndex].toString()
+        }
+
+        if (key === "iso") ret = pickNearest(val, isoList)
+        else if (key === "f_number") ret = pickNearest(val, fNumberList)
+        else if (key === "ss")
+            if (val.match(/1\/\d?\.?\d+/)) ret = pickNearestFraction(val, ssFractList)
+            else if (val.match(/\d*\.?\d+/)) ret = pickNearest(val, ssList)
+            else ret =""
+        setCameraSettings({ ...cameraSettings, [key]: ret })
     }
+
     const handler = (key, val) => {
-        setCameraSettings({...cameraSettings, [key]: val})
+        setCameraSettings({ ...cameraSettings, [key]: val })
     }
 
     const capture = async () => {
@@ -61,33 +77,34 @@ function Control({deviceId}) {
             setImageURL(url)
         } catch (e) {
             console.log(e)
-            setErrorText('Error!!!') // TODO Error Handling
-            setTimeout(() => setErrorText(''), 3000)
+            setErrorText("Error!!!") // TODO Error Handling
+            setTimeout(() => setErrorText(""), 3000)
         }
     }
 
     // TODO Input in ms -> x/y (s)
     return loaded ? (
         <div className="control">
-            <img src={imageURL} alt=""/>
+            <img src={imageURL} alt="" />
             <div className="config">
                 <div className="forms">
-                    <Form labelName="SS(ms)" id="ss" val={cameraSettings['ss']} inputHandler={handler}
-                          onFinish={onFinish} autofocus/>
-                    <Form labelName="ISO" id="iso" val={cameraSettings['iso']} inputHandler={handler}
-                          onFinish={onFinish}/>
-                    <Form labelName="F Number" id="f_number" val={cameraSettings['f_number']} inputHandler={handler}
-                          onFinish={onFinish}/>
+                    <Form labelName="SS" id="ss" val={cameraSettings["ss"]} inputHandler={handler} onFinish={onFinish} autofocus />
+                    <Form labelName="ISO" id="iso" val={cameraSettings["iso"]} inputHandler={handler} onFinish={onFinish} />
+                    <Form labelName="F Number" id="f_number" val={cameraSettings["f_number"]} inputHandler={handler} onFinish={onFinish} />
                 </div>
-                <Button handler={capture}/>
+                <Button handler={capture} />
                 <Snackbar open={!!errorText} autoHideDuration={2000}>
-                    <Alert severity="error" sx={{width: '100%'}}>{errorText}</Alert>
+                    <Alert severity="error" sx={{ width: "100%" }}>
+                        {errorText}
+                    </Alert>
                 </Snackbar>
             </div>
         </div>
-    ) : <div className="control">
-        <Loading/>
-    </div>
+    ) : (
+        <div className="control">
+            <Loading />
+        </div>
+    )
 }
 
-export default Control;
+export default Control
